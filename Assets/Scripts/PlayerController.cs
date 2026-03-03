729762cf2a448328cd3a42ffeb329722e47f79db
Md. Atiqul Islam <6184486+airOfBengal@users.noreply.github.com>
2026-03-04 01:42:29 +0600

    moving the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 10.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 inputVector = value.Get<Vector2>();
+        movementX = inputVector.x;
+        movementY = inputVector.y;
+    }
+}
